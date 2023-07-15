fdf39214ad7ecccdd681f51093c01dbb1c170233
AndreyAvdoshin <darkxbird@gmail.com>
2023-07-15 12:27:00 +0900

    feat: добавить логирование

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
 func main() {
+	//Логирование
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	//Преобразование путей к обработчикам
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -19,11 +24,17 @@ func main() {
 	// Отрезаем "/static" из пути
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Запустили сервер и проверили на ошибки
-	err := http.ListenAndServe(":8000", mux)
-	if err != nil {
-		log.Fatal(err)
+	//Инициализиуер структуру для логирования ошибок HTTP
+	srv := &http.Server{
+		Addr:     ":8000",
+		ErrorLog: errorLog,
+		Handler:  mux,
 	}
+
+	// Запустили сервер и проверили на ошибки
+	infoLog.Printf("Сервер запущен")
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 type neuteredFileSystem struct {
@@ -38,6 +49,10 @@ func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
 	}
 	// Проверяем если в пути директория и подменяем на 404
 	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
 	if s.IsDir() {
 		index := filepath.Join(path, "index.html")
 		if _, err := nfs.fs.Open(index); err != nil {
